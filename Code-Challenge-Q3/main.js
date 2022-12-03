9e2fa022ae567bd76336d79306c1df363cb63557
TCGAUD <87245593+TCGAUD@users.noreply.github.com>
2022-12-03 17:26:11 +0200

    Update main.js

--- Code-Challenge-Q3/main.js
+++ Code-Challenge-Q3/main.js
@@ -17,37 +17,24 @@ var test_data = getData(testData);
 
 
 function find_duplicates(a, b, c) {
-    var the_duplicates = [];
-
 
     for (var i = 0; i < a.length; i++) {
         for (var j = 0; j < b.length; j++) {
             if (a[i] == b[j]) {
-                console.log("proc")
                 for (var k = 0; k < c.length; k++) {
                     if (c[k] == a[i]) {
-                        for (var p = 0; p < the_duplicates.length; p++) {
-                            if (c[k] == the_duplicates[p]){
-                                continue;
-                            }
-                            else {
-                                the_duplicates.push(c[k])
-                            }
-                        }
+                        return c[k]
                     }
                 }
-
             }
-
         }
     }
-    return the_duplicates;
+    
 }
 
 
 function main() {
 
-    console.log(test_data)
 
     console.log(find_duplicates(test_data[0], test_data[1], test_data[2]))
 
