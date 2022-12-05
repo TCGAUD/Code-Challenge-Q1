fec79c2fa38bfe4f93e1d60267cd56a2a414da9f
TCGAUD <87245593+TCGAUD@users.noreply.github.com>
2022-12-05 21:52:40 +0200

    Update main.js

--- Code-Challenge-Q5/main.js
+++ Code-Challenge-Q5/main.js
@@ -15,26 +15,22 @@ function splitData(data) {
     var stop_index = data.length;
 
     var reached_process = false;
-    var instructions ;
+    var instructions;
 
     instructions = data.filter((z, i) => {
-
         if (z == '') {
             reached_process = true;
         }
 
-
         if (reached_process) {
             return z;
         }
-        
+
     }).map((k) => {//5 12
-        return([parseInt(k[5]) , parseInt(k[12]), parseInt(k[k.length -1])])
+        return ([parseInt(k[5]), parseInt(k[12]), parseInt(k[k.length - 1])])
     })
 
-
     data = data.map(item => item).reverse();
-    var Stacks = []
 
     data = data.filter((x, i) => {
 
@@ -42,16 +38,13 @@ function splitData(data) {
             stop_index = i;
         }
 
-
         if (i > stop_index + 1) {
             return x
         }
         i++;
     })
 
-
-
-    data.map((x) => {
+    data = data.map((x) => {
         var initial = 0;
         var myobj = [];
         for (var i = 0; i < x.length; i++) {
@@ -60,6 +53,7 @@ function splitData(data) {
             }
             var a = x.substring(initial, i - 1)
             if (i % 4 == 0) {
+                console.log(a)
                 myobj.push(a);
                 initial = i;
             }
@@ -68,63 +62,49 @@ function splitData(data) {
                 myobj.push(b);
             }
         }
-        Stacks.push(myobj);
+        return myobj;
     })
 
-    Stacks = Stacks.map((x) => {
 
-        x = x.map((y) => {
-            if (y != "   ") {
-                return y;
-            } else {
-                return '.';
-            }
-        })
-        return x;
-    })
 
-    var AllStacks = [];
+    return data;
 
-    for (var i = 0; i < Stacks.length; i++) {
-        var stackObj = [];
-        for (var j = 0; j < Stacks[0].length; j++) {
+    //console.log(data)
 
-            stackObj.push(Stacks[j][i]);
-        }
-        AllStacks.push(stackObj)
-    }
 
-    AllStacks = AllStacks.map((x) => {
-        x = x.filter((y) => {
-            if (y == '.') {
-            } else {
-                return y;
-            }
-        })
-        return x;
-    })
-    return [AllStacks , instructions];
+
+
 }
 
 // actual part 1
 
 function process_instructions(stacks, instruct) {
-    for (var i = 0; i < instruct.length; i++){
-        var [n,  from_i , put_i] = instruct[i];
+    for (var i = 0; i < instruct.length; i++) {
+        var [n, from_i, put_i] = instruct[i];
 
-        for (var k = 0 ; k < n; k++){
+        for (var k = 0; k < n; k++) {
             var item = stacks[(from_i - 1)].pop();
             stacks[(put_i - 1)].push(item);
         }
-        
+
     }
+    //get output for question
+    var outputString = "";
+    for (var o = 0; o < stacks.length; o++) {
+        var each_stack = stacks[o].map((x) => {
+            return (x[1])
+        })
+        outputString += each_stack.pop()
+    }
+    return outputString;
+
 }
 
 
 var appData = getData(testData)
-var [stacks, instruct] = splitData(appData)
-process_instructions(stacks, instruct);
+console.log(splitData(appData))
+// var part1_out = process_instructions(stacks, instruct);
 
-console.log(stacks)
+//console.log(part1_out)
 
 
